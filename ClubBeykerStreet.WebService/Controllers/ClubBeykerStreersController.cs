5d6f66af2932250fb3c7d1f77d81387ed382dd37
KubanychbekUuluBeksultan <50055848+KubanychbekUuluBeksultan@users.noreply.github.com>
2020-05-22 23:24:52 +0300

    Add files via upload

---
+++ ClubBeykerStreet.WebService/Controllers/ClubBeykerStreersController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ClubBeykersStreet.DomainObjects;
+
+namespace ClubBeykersStreet.WebService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClubBeykerStreetesController : ControllerBase
+    {
+        private readonly ILogger<ClubBeykerStreetesController> _logger;
+
+        public ClubBeykerStreetesController(ILogger<ClubBeykerStreetesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<ClubBeykerStreet> GetRoute()
+        {
+            return new List<ClubBeykersStreet>() 
+            { 
+                new ClubBeykersStreet() 
+                { 
+                    Id = 1,
+                    Number = "748",
+                    Name = "Ст. Ховрино - Ст. МЦД Бескудниково",
+                    Type = TransportType.Bus,
+                    Organization = new TransportOrganization()
+                    {
+                        Id = 1,
+                        Name = "ГУП \"Мосгортранс\"",
+                        TimeZone = "Europe/Moscow",
+                        WebSite = "http://mosgortrans.ru"
+                    }
+                } 
+            };
+        }
+    }
+}
---
+++ ClubBeykerStreet.WebService/DomainObjects/ClubBeykerStreet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClubBeykersStreet.DomainObjects
+{
+    public class ClubBeykerStreet : DomainObject
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+   
+        public string Datatime { get; set; }
+        public string Time { get; set; }
+    }
+}
