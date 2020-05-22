d3e8b82de7697d4a56e124f6a441ae6da8a88bee
KubanychbekUuluBeksultan <50055848+KubanychbekUuluBeksultan@users.noreply.github.com>
2020-05-22 23:24:27 +0300

    Add files via upload

---
+++ Lab1_oop/ClubBeykerStreet.WebService/DomainObjects/DomainObject.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClubBeykersStreet.DomainObjects
+{
+    public abstract class DomainObject
+    {
+        public long Id { get; set; }
+    }
+}
---
+++ Lab1_oop/ClubBeykerStreet.WebService/DomainObjects/Ports/IClubBeykerStreetRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+
+namespace ClubBeykersStreet.DomainObjects.Ports
+{
+    public interface IReadOnlyClubBeykerStreetRepository
+    {
+        Task<ClubBeykerStreet> GetClubBeykerStreet(long id);
+
+        Task<IEnumerable<ClubBeykerStreet>> GetAllClubBeykerStreet();
+
+        Task<IEnumerable<ClubBeykerStreet>> QueryClubBeykerStreet(ICriteria<ClubBeykerStreet> criteria);
+
+    }
+
+    public interface IClubBeykerStreetRepository
+    {
+        Task AddClubBeykerStreet(ClubBeykerStreet clubBeykerStreet);
+
+        Task RemoveClubBeykerStreet(ClubBeykerStreet clubBeykerStreet);
+
+        Task UpdateClubBeykerStreet(ClubBeykerStreet clubBeykerStreet);
+    }
+}
